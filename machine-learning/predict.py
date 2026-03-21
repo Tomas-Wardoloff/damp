0c4ecd2f0c55c60848a96241218232aa3e6c1d3e
diegolezcanodev <diegolezcano209@gmail.com>
2026-03-21 11:45:12 -0300

    Ajuste final modelo ml

--- machine-learning/predict.py
+++ machine-learning/predict.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 
 class MastitisPredictor:
-    def __init__(self, model_path: str = "models/nuevas-clases/mastitis_model_v4.pkl"):
+    def __init__(self, model_path: str = "models/nuevas-clases/mastitis_model_v6.pkl"):
         if not Path(model_path).exists():
             raise FileNotFoundError(
                 f"Modelo no encontrado: {model_path}\n"
